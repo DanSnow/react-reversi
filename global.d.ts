32ef6495c4b250fe686cb850c4eb2d073cf75103
DanSnow <dododavid006@gmail.com>
2020-07-17 00:07:53 +0800

    chore: Remove unused type

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,2 @@
 declare const __DEV__: boolean
 declare const VERSION: string
-
-interface Enhancer<T> {
-  (x: T): T
-}
-
-interface Window {
-  __REDUX_DEVTOOLS_EXTENSION__?: <T>() => Enhancer<T>
-}
