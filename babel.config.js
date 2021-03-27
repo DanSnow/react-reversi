50c8db1ff3dd53a5d31459a496fa773005700d6c
DanSnow <dododavid006@gmail.com>
2021-03-27 23:26:06 +0800

    chore: update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,49 @@
-module.exports = {
-  presets: [
-    '@babel/preset-typescript',
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
+/**
+ *
+ * @param {{name?: string}} caller
+ * @returns {boolean}
+ */
+function isBabelLoader(caller) {
+  return caller?.name === 'babel-loader'
+}
+/**
+ *
+ * @param {import('@babel/core').ConfigAPI} api
+ * @returns
+ */
+module.exports = function (api) {
+  const isLoader = api.caller(isBabelLoader)
+  const isProd = api.env('production')
+
+  const targets = isLoader
+    ? { browsers: ['last 2 Chrome versions', 'last 2 Firefox versions', 'last 2 Edge versions'] }
+    : { node: 'current' }
+
+  return {
+    presets: [
+      '@babel/preset-typescript',
+      [
+        '@babel/preset-env',
+        {
+          targets,
         },
-        modules: 'commonjs',
-      },
+      ],
+      ['@babel/preset-react', { runtime: 'automatic' }],
     ],
-    ['@babel/preset-react', { runtime: 'automatic' }],
-  ],
-  plugins: [
-    '@emotion',
-    [
-      'transform-imports',
-      {
-        'lodash-es': {
-          // eslint-disable-next-line no-template-curly-in-string
-          transform: 'lodash-es/${member}',
-          preventFullImport: true,
+    plugins: [
+      !isProd && require.resolve('react-refresh/babel'),
+      '@emotion',
+      [
+        'transform-imports',
+        {
+          'lodash-es': {
+            // eslint-disable-next-line no-template-curly-in-string
+            transform: 'lodash-es/${member}',
+            preventFullImport: true,
+          },
         },
-      },
+      ],
+      ['@babel/plugin-transform-runtime', { useESModules: isLoader }],
     ],
-    ['@babel/plugin-transform-runtime'],
-    '@babel/plugin-proposal-class-properties',
-    ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
-  ],
+  }
 }
--- webpack.config.babel.ts
+++ webpack.config.babel.ts
@@ -9,39 +9,6 @@ import { version } from './package.json'
 const env = process.env.NODE_ENV
 const isDev = env !== 'production'
 
-const babelConfig = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          browsers: ['last 2 Chrome versions', 'last 2 Firefox versions', 'last 2 Edge versions'],
-        },
-        exclude: ['transform-regenerator'],
-        modules: false,
-      },
-    ],
-    ['@babel/preset-react', { runtime: 'automatic' }],
-  ],
-  plugins: [
-    isDev && require.resolve('react-refresh/babel'),
-    [
-      'transform-imports',
-      {
-        'lodash-es': {
-          // eslint-disable-next-line no-template-curly-in-string
-          transform: 'lodash-es/${member}',
-          preventFullImport: true,
-        },
-      },
-    ],
-    ['@babel/plugin-transform-runtime', { useESModules: true }],
-    '@babel/plugin-proposal-class-properties',
-    ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
-    '@emotion',
-  ].filter(Boolean),
-}
-
 const baseConfig: Configuration = {
   mode: env === 'production' ? 'production' : 'development',
   entry: ['./src/index'],
@@ -60,7 +27,6 @@ const baseConfig: Configuration = {
     isDev && new ReactRefreshWebpackPlugin(),
     new DefinePlugin({
       __DEV__: env !== 'production',
-      NODE_ENV: JSON.stringify(env),
       VERSION: JSON.stringify('v' + version),
       'process.env.NODE_ENV': JSON.stringify(env),
     }),
@@ -75,10 +41,6 @@ const baseConfig: Configuration = {
         use: [
           {
             loader: require.resolve('babel-loader'),
-            options: {
-              babelrc: false,
-              ...babelConfig,
-            },
           },
         ],
         include: join(__dirname, 'src'),
