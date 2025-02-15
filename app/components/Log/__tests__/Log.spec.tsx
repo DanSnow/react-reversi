3a94cb9dbd6798a7b83c77c7fceb650b04ae7d03
DanSnow <dododavid006@gmail.com>
2025-02-15 17:54:42 +0800

    test: fix test

--- app/components/Log/__tests__/Log.spec.tsx
+++ app/components/Log/__tests__/Log.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import { describe, expect, it } from 'vitest'
 
-import { WHITE } from '../../../store'
+import { WHITE } from '~/consts'
 import { Log } from '../Log'
 
 describe('log.tsx', (): void => {
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
 import path from 'node:path'
 import react from '@vitejs/plugin-react'
+import Icons from 'unplugin-icons/vite'
 import { defineConfig } from 'vite'
+import TsConfigPath from 'vite-tsconfig-paths'
 import pkg from './package.json'
 
 export default defineConfig({
@@ -12,6 +14,11 @@ export default defineConfig({
       babel: {
         presets: ['jotai/babel/preset'],
       },
+    }),
+    TsConfigPath(),
+    Icons({
+      compiler: 'jsx',
+      jsx: 'react',
     }),
   ],
   resolve: {
