3e0811e2c387012fb92f85a7e8af05cec313e521
DanSnow <dododavid006@gmail.com>
2025-07-05 14:55:41 +0800

    chore: update shadcn components

--- src/components/ui/icon-button.tsx
+++ src/components/ui/icon-button.tsx
@@ -1,23 +1,20 @@
-import type { ButtonProps } from './button'
-
 import * as React from 'react'
 import { cn } from '~/lib/utils'
 import { Button } from './button'
 
-export interface IconButtonProps extends ButtonProps {
+export interface IconButtonProps extends React.ComponentProps<typeof Button> {
   icon: React.ReactNode
 }
 
-const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(
-  ({ className, variant, size, icon, children, ...props }, ref) => {
-    return (
-      <Button ref={ref} className={cn(className, 'gap-1')} {...props}>
-        <span>{icon}</span>
-        {children}
-      </Button>
-    )
-  },
-)
+function IconButton({ className, variant, size, icon, children, ref, ...props }: IconButtonProps) {
+  return (
+    <Button ref={ref} className={cn(className, 'gap-1')} {...props}>
+      <span>{icon}</span>
+      {children}
+    </Button>
+  )
+}
+
 IconButton.displayName = 'IconButton'
 
 export { IconButton }
